1932a7f7ab3239fe198a2482a3a2ad760bce0f15
sofiaarulina <arulinasofia507@gmail.com>
2025-03-03 07:02:41 +0300

    Add files via upload

---
+++ 7LABA.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val stringList = mutableListOf<String>()
+    stringList.add("тетрадь")
+    stringList.add("комп")
+    stringList.add("стол")
+    stringList.add("подик")
+    stringList.add("сумка")
+
+    println("Список строк: $stringList")
+}
